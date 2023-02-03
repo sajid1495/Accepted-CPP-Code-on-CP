71dd55a6bf43b0a17b9847841d2962bb515a898b
MD.SAJEDUL ISLAM <92239555+sajid1495@users.noreply.github.com>
2023-02-03 10:00:19 +0600

    Create 400m Race.cpp

---
+++ 400m Race.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b,c;
+        cin >> a >> b >> c;
+        if(a < b && a < c){
+            cout << "ALICE\n";
+        }
+        else if(b < a && b < c){
+            cout << "BOB\n";
+        }
+        else{
+            cout << "CHARLIE\n";
+        }
+    }
+	return 0;
+}
