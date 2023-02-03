3a65316365e2b9b5b17b2123acfb8c702b8f4df0
MD.SAJEDUL ISLAM <92239555+sajid1495@users.noreply.github.com>
2023-02-03 10:04:02 +0600

    Create Apple and Orange.cpp

---
+++ Apple and Orange.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n,m;
+        cin >> n >> m;
+        cout << __gcd(n,m) << endl;
+    }
+	return 0;
+}
