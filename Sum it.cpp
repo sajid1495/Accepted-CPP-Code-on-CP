b9817bae8165ac91281a2f830d221bc4f74269bc
MD.SAJEDUL ISLAM <92239555+sajid1495@users.noreply.github.com>
2023-02-03 10:02:49 +0600

    Create Sum it.cpp

---
+++ Sum it.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b,c;
+        cin >> a >> b >> c;
+        if(a+b == c){
+            cout << "YES\n";
+        }
+        else{
+            cout << "NO\n";
+        }
+    }
+	return 0;
+}
