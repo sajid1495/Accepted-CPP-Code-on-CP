18c842a01a7a0b4fd51528d26b68e27ea27b05a0
MD.SAJEDUL ISLAM <92239555+sajid1495@users.noreply.github.com>
2023-02-04 11:21:22 +0600

    Create Balance Brackets.cpp

---
+++ Balance Brackets.cpp
@@ -0,0 +1,87 @@
+/******************Bismillahir Rahmanir Rahim********************
+ Name : SAJEDUL ISLAM
+ CF_Handle : SaJiD_96
+ Institution : Rajshahi University Of Engineering & Techoonology
+ E-Mail : m.sajid1495@gmail.com
+ ***************************************************************/
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(a,b,c)              for(int i = a; i < b; i += c)
+#define all(cont)               cont.begin(), cont.end()
+#define rall(cont)              cont.end(), cont.begin()
+#define FOREACH(it, l)          for(auto it = l.begin(); it != l.end(); it++)
+#define MP                      make_pair
+#define PB                      push_back
+#define INF                     (int)1e9
+#define EPS                     1e-9
+#define PI                      3.1415926535897932384626433832795
+#define MOD                     1000000007
+#define read(type)              readInt<type>()
+#define YES                     cout << "YES" << endl
+#define NO                      cout << "NO" << endl
+
+const double pi=acos(-1.0);
+
+typedef pair<int, int>          PII;
+typedef vector<int>             VI;
+typedef vector<string>          VS;
+typedef vector<PII>             VII;
+typedef vector<VI>              VVI;
+typedef map<int,int>            MPII;
+typedef set<int>                SETI;
+typedef multiset<int>           MSETI;
+typedef long int                int32;
+typedef unsigned long int       uint32;
+typedef long long int           int64;
+typedef unsigned long long int  uint64;
+
+
+  
+int main(){
+    int n;
+    cin >> n;
+    while(n--){
+        stack <char> s;
+        string str;
+        cin >> str;
+        int flag = 1;
+        for(int i = 0; i < str.size(); i++){
+            if(str[i] == '(' || str[i] == '{'  || str[i] == '[' ){
+                s.push(str[i]);
+            }
+            else{
+                if(!s.empty()){
+                    if(str[i] == ')' && s.top() == '('){
+                        s.pop();
+                    }
+                    else if(str[i] == '}' && s.top() == '{'){
+                        s.pop();
+                    }
+                    else if(str[i] == ']' && s.top() == '['){
+                        s.pop();
+                    }
+                    else{
+                        flag = 0;
+                        break;
+                    }
+                }
+                else{
+                    flag = 0;
+                    break; 
+                }
+            }
+        }
+        if(s.empty() && flag == 1){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
